3a3a33b6cfcee2a20c9455ca47b3fd5bc5ebc5f1
Sam Corcos <sam@corcos.io>
2015-07-27 02:57:15 +0200

    adding first card

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,9 +5,37 @@ Home = React.createClass({
 
     }
   },
+  getInitialState() {
+    return {
+
+    }
+  },
   render() {
     return (
-      <div>home</div>
+      <div>
+        <div className="list card">
+          <div className="item item-avatar">
+            <img src="https://avatars0.githubusercontent.com/u/1794527?v=3&s=460" />
+            <h2>Title of card</h2>
+            <p>Content of card</p>
+          </div>
+
+          <div className="item item-image">
+            <img src="https://avatars0.githubusercontent.com/u/1794527?v=3&s=460" />
+          </div>
+
+          <div className="item tabs tabs-secondary tabs-icon-left">
+           <a className="tab-item" href="#">
+             <i className="icon ion-thumbsup balanced"></i>
+             Like
+           </a>
+           <a className="tab-item" href="#">
+             <i className="icon ion-thumbsdown assertive"></i>
+             Dislike
+           </a>
+         </div>
+        </div>
+      </div>
     )
   }
 })
